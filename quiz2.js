40dbaa80cf62d4988ad8d647f28bcfe0def36b80
khaos <khaos991213@gmail.com>
2021-03-29 19:23:03 +0800

    final

---
+++ quiz2.js
@@ -0,0 +1,5 @@
+window.addEventListener("keydown", function(event) {
+    const p = document.createElement("p");
+    p.textContent = event.code;
+    document.getElementById("output").appendChild(p);
+}, true);
